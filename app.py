bc590061556f1b4e97ef2eeb0dfb3000a0831b3a
fidwu <fidellapwu@gmail.com>
2025-05-03 11:46:39 -0700

    Add patients and statuses pages

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def home():
 
 
 @app.route("/clinics", methods=["GET"])
-def bsg_people():
+def clinics():
     try:
         dbConnection = db.connectDB()  # Open our database connection
 
@@ -45,6 +45,53 @@ def bsg_people():
         if "dbConnection" in locals() and dbConnection:
             dbConnection.close()
 
+@app.route("/patients", methods=["GET"])
+def patients():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT patientId, firstName, lastName, email, dateOfBirth, gender, CONCAT('Capital Family Clinic in ', Clinics.city, ', ', Clinics.state) AS clinicName \
+                FROM Patients \
+                JOIN Clinics ON Patients.clinicId = Clinics.clinicId;"
+        patients = db.query(dbConnection, query1).fetchall()
+
+        # Render the patients.j2 file, and also send the renderer patients information
+        return render_template(
+            "patients.j2", patients=patients
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+@app.route("/statuses", methods=["GET"])
+def statuses():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT statusId, status FROM Statuses ORDER BY statusId;"
+        statuses = db.query(dbConnection, query1).fetchall()
+
+        # Render the statuses.j2 file, and also send the renderer statuses information
+        return render_template(
+            "statuses.j2", statuses=statuses
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
 
 
 # ########################################
