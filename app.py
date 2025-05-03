66190800e23712f1c265d0b2f4cd226ffacfcf78
Jacob Durham <jacobgdurham@gmail.com>
2025-05-03 15:15:54 -0700

    added appointments to app.py, fixed appointments in main.j2

--- app.py
+++ app.py
@@ -31,9 +31,34 @@ def bsg_people():
         query1 = "SELECT clinicId, address, city, state, postalCode, phoneNumber FROM Clinics;"
         clinics = db.query(dbConnection, query1).fetchall()
 
+        query2 = "SELECT appointmentId, date, time, Appointments.clinicId, firstName, lastName, status FROM Appointments JOIN Patients ON Appointments.patientId = Patients.patientId JOIN Statuses ON Appointments.statusId = Statuses.statusId;"
+        appointments = db.query(dbConnection, query2).fetchall()
+
         # Render the clinics.j2 file, and also send the renderer clinics information
         return render_template(
             "clinics.j2", clinics=clinics
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+            
+@app.route("/appointments", methods=["GET"])
+def get_appointments():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        query2 = "SELECT appointmentId, date, time, Appointments.clinicId, firstName, lastName, status FROM Appointments JOIN Patients ON Appointments.patientId = Patients.patientId JOIN Statuses ON Appointments.statusId = Statuses.statusId;"
+        appointments = db.query(dbConnection, query2).fetchall()
+
+        # Render the appointments.j2 file, and also send the renderer appointments information
+        return render_template(
+            "appointments.j2", appointments=appointments
         )
 
     except Exception as e:
