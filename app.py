14ae61359f2eccc76f2249f3cc2ade16b995830e
Jacob Durham <jacobgdurham@gmail.com>
2025-05-03 15:47:37 -0700

    fixed the appointments query and the table in appointments.j2

--- app.py
+++ app.py
@@ -31,9 +31,6 @@ def bsg_people():
         query1 = "SELECT clinicId, address, city, state, postalCode, phoneNumber FROM Clinics;"
         clinics = db.query(dbConnection, query1).fetchall()
 
-        query2 = "SELECT appointmentId, date, time, Appointments.clinicId, firstName, lastName, status FROM Appointments JOIN Patients ON Appointments.patientId = Patients.patientId JOIN Statuses ON Appointments.statusId = Statuses.statusId;"
-        appointments = db.query(dbConnection, query2).fetchall()
-
         # Render the clinics.j2 file, and also send the renderer clinics information
         return render_template(
             "clinics.j2", clinics=clinics
@@ -53,8 +50,8 @@ def get_appointments():
     try:
         dbConnection = db.connectDB()  # Open our database connection
 
-        query2 = "SELECT appointmentId, dateTime, Appointments.clinicId, firstName, lastName, status FROM Appointments JOIN Patients ON Appointments.patientId = Patients.patientId JOIN Statuses ON Appointments.statusId = Statuses.statusId;"
-        appointments = db.query(dbConnection, query2).fetchall()
+        appointmentsquery = "SELECT appointmentId, dateTime, Appointments.clinicId, Patients.firstName, Patients.lastName, Statuses.status FROM Appointments JOIN Patients ON Appointments.patientId = Patients.patientId JOIN Statuses ON Appointments.statusId = Statuses.statusId"
+        appointments = db.query(dbConnection, appointmentsquery).fetchall()
 
         # Render the appointments.j2 file, and also send the renderer appointments information
         return render_template(
