7576ee1a87b723f25f9017bd9e68cbf9888763f2
Jacob Durham <jacobgdurham@gmail.com>
2025-05-04 16:51:37 -0700

    added event listener for the submit button, changed the appointments submission form

--- static/script.js
+++ static/script.js
@@ -6,4 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("button.close").addEventListener("click", (event) => {
         document.querySelector(".add-form-popup").classList.remove("active");
     });
+
+    document.querySelector("button.submit").addEventListener("click", (event) =>{
+        event.preventDefault();
+        document.querySelector(".add-form-popup form").submit();
+    });
 });
