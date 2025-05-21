5818a73fe11641c13ee2047fa106458433b5b3d0
fidwu <fidellapwu@gmail.com>
2025-05-20 22:38:17 -0700

    Fix for reset popup

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".add-form-popup").classList.add("active");
     });
 
-    document.querySelector("button.close").addEventListener("click", (event) => {
+    document.querySelector("button.close")?.addEventListener("click", (event) => {
         document.querySelector(".add-form-popup").classList.remove("active");
         window.location.href = window.location.href.split("?").slice(0, -1).toString()
     });
@@ -16,16 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    document.getElementById('cancel').addEventListener('click', function() {
+    document.getElementById('cancel')?.addEventListener('click', function() {
         document.querySelector('.delete-form-popup').classList.remove('active');
     });
 
     document.querySelector(".reset-trigger").addEventListener("click", () => {
-        document.querySelector('.reset-form-popup').classList.add('active');
+        document.querySelector('.reset-popup').classList.add('active');
     });
 
     document.querySelector('.cancel').addEventListener('click', function() {
-        document.querySelector('.reset-form-popup').classList.remove('active');
+        document.querySelector('.reset-popup').classList.remove('active');
     });
 
     // document.querySelector("button.submit").addEventListener("click", (event) =>{
